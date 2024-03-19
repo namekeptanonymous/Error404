cc66ca0dfcbd89bec3a1f2e3d00c55792450b62a
Aaditya-Golash <aadigolash10@outlook.com>
2024-03-18 17:39:04 -0700

    commit to make new branch

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,10 +1,10 @@
-/* eslint-disable no-undef */
-import { defineConfig } from 'vite'
-
 
 // https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [React()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
