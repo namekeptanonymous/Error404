3144aadf7bd0be0096dad61b4a6acd4083d55789
namekeptanonymous <113662431+namekeptanonymous@users.noreply.github.com>
2024-03-20 19:37:51 -0700

    Added base Hero code + changed tab title & favicon

--- discord-clone/src/components/Hero.jsx
+++ discord-clone/src/components/Hero.jsx
@@ -1,7 +1,21 @@
+import React from 'react'
+
 function Hero() {
-    return (
-        <div><h1>hello hero</h1></div>
-    );
+  return (
+    <div className="bg-[#b2d5ef] pb-8 md:pb-0">
+      <div className="p-7 py-9 h-screen md:h-[83vh]">
+        <div className="flex flex-col gap-7">
+          <h1>Your place to yap!</h1>
+          <h2>
+            Whether you’re part of a school club, gaming group, worldwide art
+            community, or just a handful of friends that want to spend time
+            together, Discord makes it easy to talk every day and hang out more
+            often.
+          </h2>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Hero
