1767dc4cfda5c56f0203557ee8187a9d2464d400
namekeptanonymous <113662431+namekeptanonymous@users.noreply.github.com>
2024-03-25 16:35:45 -0700

    Added changes to fix previously broken UI and code

--- discord-clone/src/components/Home.jsx
+++ discord-clone/src/components/Home.jsx
@@ -5,7 +5,8 @@ import { Navigate } from 'react-router-dom';
 import ServerIcon from './ServerIcon';
 import {ChevronDownIcon,PlusIcon} from "@heroicons/react/24/outline";
 import { useCollection } from "react-firebase-hooks/firestore";
-import { collection } from 'firebase/firestore';
+import { collection, doc, setDoc } from 'firebase/firestore';
+import Channel from './Channel.jsx'
 
 function Home() {
   const [user] = useAuthState(auth);
@@ -17,19 +18,19 @@ function Home() {
 
   const[channels]=useCollection(collection(db, "channels"));
 
-  const handleAddChannel =() =>{
-    const channelName =prompt("Enter a new channel name");
-    if(channelName){
-      db.collection("channels").add({
-        channnelName: channelName,
-      });
+  const handleAddChannel = () => {
+    const channelName = prompt("Enter a new channel name");
+    if (channelName) {
+      // Use Firestore's doc() and setDoc() methods
+      const channelRef = doc(db, "channels", channelName);
+      setDoc(channelRef, { channelName });
     }
   };
   return (
     <>
       <div className="flex h-screen">
-        <div className="flex flex-col space-y-3 bg-discord-serversBg">
-          <div className="server-default hover:bg-discord-purple">
+        <div className="flex flex-col space-y-3 bg-discord_serversBg p-3 min-w-max">
+          <div className="server-default hover:bg-discord_purple">
             <img src="../src/images/chatterbox.png" alt="" className="h-5" />
           </div>
           <hr className="border-gray-700 border w-8 mx-auto" />
@@ -41,7 +42,7 @@ function Home() {
           <div className="server-default hover:bg-discord_green group">
             <PlusIcon className="text-discord_green h-7 group-hover:text-white"/>
           </div>
-
+        </div>
           <div className="bg-discord_channelsBg flex flex-col min-w-max"> {/* Assuming you wanted to use a background class here */}
             <h2 className= "flex text-white font-bold text-sm items-center justify-between border-b  border-gray-800 p-4 hover:bg-discord_serverNameHoverBg cursor-pointer">Official Server...<ChevronDownIcon className=" h-5 ml-2"/>
             </h2>
@@ -53,13 +54,14 @@ function Home() {
               </div>
               <div className ="flex flex-col space-y-2 px-2 mb-4">
                 {channels?.docs.map((doc)=>(
-                  <Channel key={doc.id} channelName={doc.data().channelName} />
+                  <Channel
+                    key={doc.id}
+                    id={doc.id}
+                    channelName={doc.data().channelName} />
                 ))}
-                
               </div>
             </div>
           </div>
-        </div>
       </div>
     </>
   );
