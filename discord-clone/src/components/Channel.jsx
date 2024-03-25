818badf702da5f15bf8ede1e604a8f59eeed2df1
Kenneth Abadi <kenneth.putra25@gmail.com>
2024-03-25 14:38:26 -0700

    Added channel switching and fixed minor problem

--- discord-clone/src/components/Channel.jsx
+++ discord-clone/src/components/Channel.jsx
@@ -1,9 +1,27 @@
 import {HashtagIcon} from "@heroicons/react/outline";
+import {useDispatch} from "react-redux"
+import {useHistory} from "react-router-dom"
+import { setChannelInfo } from "../features/channelSlice";
+
 function Channel({id,channelName}){
-    const setChannel =() =>{};
+
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const setChannel =() =>{
+            dispatch(setChannelInfo({
+                channelId: id,
+                channelName: channelName,
+            })
+        );
+        history.push('/channels/${id}');
+    };
+    
+
     return(
-        <div className= "font-medium flex items-center cursor-pointer hover:bg-discord_channelHoverBg p-1 rounded-md hover:text-white">
-           <HashtagIcon className = "h-5 mr-2" onClick ={setChannel}/> {channelName}
+        <div className= "font-medium flex items-center cursor-pointer hover:bg-discord_channelHoverBg p-1 rounded-md hover:text-white" onClick ={setChannel}>
+           <HashtagIcon className = "h-5 mr-2" /> 
+           {channelName}
         </div>
     )
 }
