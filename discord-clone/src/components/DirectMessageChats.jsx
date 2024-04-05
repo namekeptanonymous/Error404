06d2f8221203b8896d05ca3077b2b76ad61769d3
Ariq Muldi <ariqmuldi@gmail.com>
2024-04-05 01:32:57 -0700

    Implement Chats to have the chats with other people

--- discord-clone/src/components/DirectMessageChats.jsx
+++ discord-clone/src/components/DirectMessageChats.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useEffect } from 'react'; //
+import chatterboxImage from '../images/chatterbox.png';
+import { auth, db } from '../firebase';
+import { useNavigate } from 'react-router-dom'; //
 
 const DirectMessageChats = () => {
+  const [user] = useAuthState(auth);
+
   return (
-    <div>
-      Chats
+    <div className = "chats">
+      <div className = "userChat">
+          <img src={user?.photoURL} alt="User Logo" />
+
+          <div className = "userChatInfo">
+            <span>{user.displayName}</span>
+            <p> Hello World! </p>
+          </div>
+
+      </div>
+
+      <div className = "userChat">
+          <img src={user?.photoURL} alt="User Logo" />
+
+          <div className = "userChatInfo">
+            <span>{user.displayName}</span>
+            <p> Hello World! </p>
+          </div>
+
+      </div>
+
+      <div className = "userChat">
+          <img src={user?.photoURL} alt="User Logo" />
+
+          <div className = "userChatInfo">
+            <span>{user.displayName}</span>
+            <p> Hello World! </p>
+          </div>
+
+      </div>
+
     </div>
   )
 }
--- discord-clone/src/components/DirectMessageSidebar.jsx
+++ discord-clone/src/components/DirectMessageSidebar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import DirectMessageNavbar from './DirectMessageNavbar';
 import DirectMessageSearch from './DirectMessageSearch';
+import DirectMessageChats from './DirectMessageChats';
 const DirectMessageSidebar = () => {
 
     return (
         <div className = "sidebar">
             <DirectMessageNavbar/>
             <DirectMessageSearch/>
+            <DirectMessageChats/>
         </div>
     )
 }
