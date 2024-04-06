0a1e931129f8c2ecb81ffab897c4e10479f94bd1
Ariq Muldi <ariqmuldi@gmail.com>
2024-04-05 19:33:23 -0700

    Implemented more of direct message chat and chat context

--- discord-clone/src/components/DirectMessageChats.jsx
+++ discord-clone/src/components/DirectMessageChats.jsx
@@ -1,47 +1,46 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useEffect } from 'react'; //
 import chatterboxImage from '../images/chatterbox.png';
 import { auth, db } from '../firebase';
 import { useNavigate } from 'react-router-dom'; //
+import { doc, onSnapshot } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
 
 const DirectMessageChats = () => {
-  const [user] = useAuthState(auth);
+  
+  const [chats, setChats] = useState([]);
+  const [currentUser] = useAuthState(auth);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid])
 
   return (
     <div className = "chats">
-      <div className = "userChat">
-          <img src={user?.photoURL} alt="User Logo" />
+
+    {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+      <div className = "userChat" key={chat[0]}>
+          <img src={chat[1].userInfo.photoURL} alt="User Logo" />
 
           <div className = "userChatInfo">
-            <span>{user.displayName}</span>
-            <p> Hello World! </p>
+            <span>{chat[1].userInfo.name}</span>
+            <p>{chat[1].lastMessage?.text}</p>
           </div>
 
       </div>
-
-      <div className = "userChat">
-          <img src={user?.photoURL} alt="User Logo" />
-
-          <div className = "userChatInfo">
-            <span>{user.displayName}</span>
-            <p> Hello World! </p>
-          </div>
-
-      </div>
-
-      <div className = "userChat">
-          <img src={user?.photoURL} alt="User Logo" />
-
-          <div className = "userChatInfo">
-            <span>{user.displayName}</span>
-            <p> Hello World! </p>
-          </div>
-
-      </div>
-
+    ))}
     </div>
-  )
-}
+  );
+};
 
 export default DirectMessageChats
